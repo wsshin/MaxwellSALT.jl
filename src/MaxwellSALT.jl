a93528b34c1db8eefc687140cd17376358a0cea1
Wonseok Shin <wsshin97@gmail.com>
2021-06-03 08:37:40 -0400

    Use AbbreviatedTypes

--- src/MaxwellSALT.jl
+++ src/MaxwellSALT.jl
@@ -1,39 +1,14 @@
 module MaxwellSALT
 
 using Reexport
-@reexport using MaxwellFDM
+@reexport using MaxwellWave
 using SALTBase, LinearAlgebra
 using SparseArrays: SparseMatrixCSC
 using SuiteSparse.UMFPACK: UmfpackLU
+using AbbreviatedTypes
 
-# Below, use Int instead of Int64 for compatibility with 32-bit systems (e.g., x86 in appveyor.yml).
-const Float = typeof(0.0)  # use Float = Float128 for quadruple precision in the future
-const CFloat = Complex{Float}
-
-const Tuple2 = NTuple{2}
-
-const VecComplex = Vector{CFloat}
-
-const AbsVec = AbstractVector
-const AbsMat = AbstractMatrix
-
-const AbsVecBool = AbsVec{Bool}
-const AbsVecFloat = AbsVec{Float}
-const AbsVecComplex = AbsVec{CFloat}
-const AbsMatComplex = AbsMat{CFloat}
-
-const AbsVecInteger = AbsVec{<:Integer}
-const AbsVecReal = AbsVec{<:Real}
-const AbsVecNumber = AbsVec{<:Number}
-const AbsMatNumber = AbsMat{<:Number}
-
-const MatParam = Union{Number,AbsVecNumber,AbsMatNumber}
-
-const MatComplex = Matrix{CFloat}
 const SparseMatComplex = SparseMatrixCSC{CFloat,Int}
-
 const FactComplex = Factorization{CFloat}
-
 const SparseLUComplex = UmfpackLU{CFloat,Int}
 const DenseLUComplex = LU{CFloat,MatComplex}
 
